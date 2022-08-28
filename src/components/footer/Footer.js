899ec28ca239cbd2c909739f5886ea848e8f2aa5
MarcinMaster <m.luba505@gmail.com>
2022-08-28 08:58:51 +0200

    Added a styled Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,42 +1,39 @@
 import React from "react";
-import { Grid, Container, Box, Link } from "@mui/material";
+import { Grid, Container, Box, Typography } from "@mui/material";
+import { LocalPhone, Email, FmdGood } from "@mui/icons-material";
+import useStyles from "./Styles";
 
 const Footer = () => {
+  const classes = useStyles();
   return (
     <footer>
-      <Box px={{xs: 3, sm: 10 }} py={{xs: 5, sm: 10}} sx={{ bgcolor: "#37393b" }} color='white'>
+      <Box
+        px={{ xs: 3, sm: 9 }}
+        py={{ xs: 5, sm: 7 }}
+        sx={{ bgcolor: "#37393b" }}
+      >
         <Container maxWidth="lg">
-          <Grid  container spacing={5}>
-            <Grid  item xs={12} sm={4}>
-              <Box borderBottom={1}> Kontakt</Box>
-              <Box >
-                <Link color="inherit">Zapraszam do kontaktu</Link>
-              </Box>
-              <Box>
-                <Link color="inherit">Formularz Kontaktowy</Link>
-              </Box>
-              <Box>
-                <Link color="inherit">Numer telefonu</Link>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}> Oferta</Box>
-              <Box>
-                <Link color="inherit">Kontakt</Link>
-              </Box>
-              <Box>
-                <Link color="inherit">Kontakt</Link>
-              </Box>
-              <Box>
-                <Link color="inherit">Kontakt</Link>
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4} md={4}>
+              <Box sx={{ borderRight: 2 }} className={classes.boxcontainer}>
+                <Box>
+                  <Typography fontWeight={700}>Konstantyów Łódzki</Typography>
+                </Box>
+                <Box className={classes.boxitem}>
+                  <FmdGood /> Jana Pawła II 34
+                </Box>
+                <Box className={classes.boxitem}>
+                  <LocalPhone />
+                  784840722
+                </Box>
+                <Box className={classes.boxitem}>
+                  <Email fontSize="small" />
+                  Biuro@filipkredyty.pl
+                </Box>
               </Box>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>^</Box>
-              <Box>
-                <Link color="inherit">Kontakt</Link>
-              </Box>
-            </Grid>
+            <Grid item xs={12} sm={4} md={4}></Grid>
+            <Grid item xs={12} sm={4} md={4}></Grid>
           </Grid>
         </Container>
       </Box>
