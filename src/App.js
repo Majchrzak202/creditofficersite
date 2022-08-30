f876020a6e0223e30ae93dd82161d7a816e1502c
MarcinMaster <m.luba505@gmail.com>
2022-08-30 19:52:24 +0200

    Added a Router to About and COntact Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import HomePage from "./pages/HomePage";
+import SocialNavbar from "./components/social-navbar/SocialNavbar";
+import Copyrigth from "./components/footer/Copyrigth";
+import ProductPage from "./pages/product-page/ProductPage";
+import { Routes } from "react-router";
+import { Route } from "react-router";
+import AboutMe from "./components/screens/about/AboutMe";
+import ContactPage from "./components/screens/contact/ContactPage";
 
 function App() {
-  return <div className="App">BOOM</div>;
+  return (
+    <div className="App">
+      <SocialNavbar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/kredyty/:id" element={<ProductPage />} />
+        <Route path="/omnie" element={<AboutMe />}></Route>
+        <Route path="/kontakt" element={<ContactPage />}></Route>
+      </Routes>
+      <Footer />
+      <Copyrigth />
+    </div>
+  );
 }
 
 export default App;
