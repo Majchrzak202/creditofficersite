cdcf289954c54b3ac641aef4a8edf05176025420
MarcinMaster <m.luba505@gmail.com>
2022-09-07 09:00:35 +0200

    Little style adjustment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,33 @@
 import React from "react";
-
+import Offer from "../components/screens/offer/Offer";
+import WhyMe from "../components/screens/why-me/WhyMe";
+import Recomendations from "../components/screens/recomendations/Recomendations";
 import { makeStyles } from "@material-ui/core";
+import ContactForm from "../components/screens/contact/ContactForm";
+import MainPage from "../components/screens/main/MainPage";
 
 const useStyles = makeStyles({
   homepage: {
     minHeight: "100vh",
-    dispaly: 'flex', 
-    justifyContent: 'center',
-    alignItems: 'center',
+    dispaly: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    paddingTop: '-10px'
   },
 });
 
 const HomePage = () => {
   const classes = useStyles();
-  return <div className={classes.homepage}>Yo yo yo </div>;
+  return (
+    <div className={classes.homepage}>
+      <MainPage />
+      <WhyMe />
+      <Offer />
+      <Recomendations />
+      <ContactForm />
+    </div>
+  );
 };
 
 export default HomePage;
