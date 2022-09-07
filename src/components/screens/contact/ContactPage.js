c5f4de7d2058be200d8fc4c67a0262836afa878a
MarcinMaster <m.luba505@gmail.com>
2022-09-07 08:58:45 +0200

    Added a Contact Form Page with Image and Contact Form

--- src/components/screens/contact/ContactPage.js
+++ src/components/screens/contact/ContactPage.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 import ContactImg from "./../../../assets/ContactPhoto.jpeg";
-import { Grid, CardMedia } from "@mui/material";
+import { Grid } from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  image: {
+    width: "400px",
+    height: "599px",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+    transition: "all .3s ease-in",
+    "&:hover": {
+      boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
+    },
+  },
+});
+
 
 const ContactPage = () => {
+  const classes = useStyles();
+
   return (
-    <div style={{ marginTop: "100px", marginBottom: '60px' }} className="main">
+    <div style={{ marginTop: "100px", marginBottom: "60px" }} className="main">
       <Grid spacing={1} container>
         <Grid item lg={6} md={6} sm={12} xs={12}>
-          <img style={{width: '400px', height: '599px'}} src={ContactImg} />
+          <img className={classes.image} src={ContactImg} />
         </Grid>
-        <Grid style={{marginTop: '50px'}} item lg={6} md={6} sm={12} xs={12}>
+        <Grid style={{ marginTop: "50px" }} item lg={6} md={6} sm={12} xs={12}>
           <ContactForm />
         </Grid>
       </Grid>
