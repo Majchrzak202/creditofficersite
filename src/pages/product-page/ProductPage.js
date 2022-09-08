8c9b9e2dce6dd78b3b57cf55fca69336be6a7d21
MarcinMaster <m.luba505@gmail.com>
2022-09-08 19:48:58 +0200

    Added a Product Page with styling and useParams to fetch data about Products and pair it with Router

--- src/pages/product-page/ProductPage.js
+++ src/pages/product-page/ProductPage.js
@@ -2,7 +2,8 @@ import React from "react";
 import useStyles from "./Styles";
 import { useParams } from "react-router";
 import offerSegments from "../../components/consts/Offers";
-import { Paper, Box, Card, Typography, Button } from "@mui/material";
+import { Paper, Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -11,37 +12,28 @@ const ProductPage = () => {
   const title = offerSegments[id - 1].title;
   const backgroundImage = offerSegments[id - 1].backgroundImage;
   const longDescription = offerSegments[id - 1].longDescription;
-  ;
-
-  console.log(title);
-
   return (
     <Box>
       <Box className={classes.page}>
         <Paper
           sx={{
-            background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${backgroundImage})`,
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${backgroundImage})`,
           }}
           className={classes.paperContainer}
-        ></Paper>
-      </Box>
-      <Box className={classes.secondPage}>
-        <Card
-          sx={{ bgcolor: "rgba(220, 220, 220, 0)" }}
-          className={classes.card}
         >
           <Typography style={{ color: "white" }} variant="h3" fontWeight={700}>
             {title}
           </Typography>
-        </Card>
+        </Paper>
       </Box>
+
       <Box className={classes.thirdPage}>
         <Box className={classes.description}>
-          <Typography align="inherit">
-            {longDescription}
-          </Typography>
+          <Typography align="inherit">{longDescription}</Typography>
         </Box>
-        <Button style={{ marginTop: "50px" }}>Skontaktuj się ze mną</Button>
+        <Link to="/kontakt">
+          <Button style={{ marginTop: "50px" }}>Skontaktuj się ze mną</Button>
+        </Link>
       </Box>
     </Box>
   );
