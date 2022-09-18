752694e729e9d0e91c71e5fd7e323929bfc0eac3
MarcinMaster <m.luba505@gmail.com>
2022-09-18 10:20:13 +0200

    Added a handler to remove socoail navabr while scrolling down the site

--- src/components/social-navbar/SocialNavbar.js
+++ src/components/social-navbar/SocialNavbar.js
@@ -1,51 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useStyles from "./Styles";
 import { Container, Toolbar, Grid } from "@mui/material";
 import { LocalPhone, Email, WatchLater } from "@mui/icons-material";
 
 const SocialNavbar = () => {
+  const [show, setShow] = useState(true);
   const classes = useStyles();
+
+  const showHandler = () => {
+    if (window.scrollY > 50) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", showHandler);
+  }, []);
+
   return (
-    <Container
-      sx={{
-        bgcolor: "black",
-        height: "50px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexShrink: 0,
-      }}
-      maxWidth="xxl"
-    >
-      <Toolbar>
-        <Grid className={classes.container} container>
-          <Grid
-            className={classes.item}
-            display={{ xs: "flex", sm: "flex", lg: "flex", md: "flex" }}
-            item
-          >
-            <LocalPhone fontSize="small" />
-            784840722
-          </Grid>
-          <Grid
-            className={classes.item}
-            display={{ xs: "flex", sm: "flex", lg: "flex", md: "flex" }}
-            item
-          >
-            <Email fontSize="small" />
-            Biuro@filip.pl
-          </Grid>
-          <Grid
-            display={{ xs: "none", sm: "flex", lg: "flex", md: "flex" }}
-            className={classes.item}
-            item
-          >
-            <WatchLater fontSize="small" />
-            Pn - Pt 9:00 - 18:00
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </Container>
+    <>
+      {show && (
+        <Container
+          sx={{
+            backgroundColor: "black",
+            height: "50px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexShrink: 0,
+            width: "101%",
+          }}
+          maxWidth="xxl"
+        >
+          <Toolbar>
+            <Grid className={classes.container} container>
+              <Grid
+                className={classes.item}
+                display={{ xs: "flex", sm: "flex", lg: "flex", md: "flex" }}
+                item
+              >
+                <LocalPhone fontSize="small" />
+                784840722
+              </Grid>
+              <Grid
+                className={classes.item}
+                display={{ xs: "flex", sm: "flex", lg: "flex", md: "flex" }}
+                item
+              >
+                <Email fontSize="small" />
+                Biuro@filip.pl
+              </Grid>
+              <Grid
+                display={{ xs: "none", sm: "flex", lg: "flex", md: "flex" }}
+                className={classes.item}
+                item
+              >
+                <WatchLater fontSize="small" />
+                Pn - Pt 9:00 - 18:00
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </Container>
+      )}
+    </>
   );
 };
 
