b1523827ae449aaec03278fbc89ba44991ed5ab2
MarcinMaster <m.luba505@gmail.com>
2022-09-18 10:21:20 +0200

    Moved the styling into separate Styles.js comp

---
+++ src/components/screens/recomendations/Styles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  reco: {
+    display: "flex",
+    minHeight: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: '#F5F5F5',
+    paddingTop: '15px'
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  item: {
+    width: "80vw",
+    backgroundColor: "#D3D3D3",
+    marginTop: "10px",
+    marginBottom: "10px",
+    padding: "20px",
+    borderRadius: "8px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export default useStyles;
